6e740894aa837e23fedf09b878a439804296d24d
pedro <pedrogermano232@gmail.com>
2024-02-09 01:08:35 -0300

    chore: add header and welcome

--- smartfit-challenge/src/App.tsx
+++ smartfit-challenge/src/App.tsx
@@ -1,12 +1,13 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 
 export const queryClient = new QueryClient()
+import  Welcome  from './components/welcome'
 
 
 function App() {
   return (
    <QueryClientProvider client={queryClient}>
-
+    <Welcome />
    </QueryClientProvider>
   )
 }
---
+++ smartfit-challenge/src/components/welcome.tsx
@@ -0,0 +1,21 @@
+const welcome = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold uppercase">
+        Reabertura
+        <br />
+        Smart Fit
+      </h1>
+
+      <div className="h-4 w-36 bg-brand-darkGrey">
+      </div>
+        <p>
+          O horário de funcionamento das nossas unidades está seguindo os
+          decretos de cada município. Por isso, confira aqui se a sua unidade
+          está aberta e as medidas de segurança que estamos seguindo.
+        </p>
+    </div>
+  );
+};
+
+export default welcome;
